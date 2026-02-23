3e377e406c1deb98cb1b4e5a106462c0fdb5ba53
Rafael de Araujo Cerqueira <racsx975@gmail.com>
2026-02-23 02:52:45 -0300

    feat: lógica de captura de inputs com useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
+
+  const [usuario, setUsuario] = useState('')
+  const [senha, setSenha] = useState('')
+
+  const acessLogin = () => {
+    if (usuario === "")
+      alert ("Por favor, diga seu nome viajante!");
+    if (usuario === "Mestre" && senha === "1234")
+      alert ("Bem-vindo à Dungeon, "+ usuario + "!");
+    else
+      alert("Usuário ou Senha incorretos, viajante.")
+  }
 
   return (
     <div className="app-container">
@@ -10,15 +23,25 @@ function App() {
         </div>
         <div className="login-group input-row">
           <label className="label-login">Usuário: </label>
-          <input type="text" placeholder="Digite seu nome, viajante..." />
+          <input
+            type="text"
+            placeholder="Digite seu nome, viajante..."
+            value={usuario}
+            onChange={(e) => setUsuario(e.target.value)}
+          />
         </div>
         <div className="login-group input-row">
           <label className="label-login">Senha: </label>
-          <input type="password" placeholder="Digite seu feitiço secreto..." />
+          <input 
+          type="password" 
+          placeholder="Digite seu feitiço secreto..."
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          />
         </div>
 
         <div className="buttons-group">
-          <button className="login-button">Entrar na Dungeon</button>
+          <button className="login-button" onClick={acessLogin}>Entrar na Dungeon</button>
           <button className="signin-button">Criar conta</button>
         </div>
 
